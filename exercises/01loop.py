46792739e4352c966b4326751d1695d142c7f474
sschneeberg <simone.m.schneeberg@gmail.com>
2021-01-07 18:12:55 -0500

    01 completed

--- exercises/01loop.py
+++ exercises/01loop.py
@@ -13,3 +13,17 @@
 # > Hello there
 # > Hello there
 # > Hello there
+
+def p_times(str, n):
+    if n <= 0 : return
+    arr = [str] * n
+    printStr = "\n".join(arr)
+    print(printStr)
+
+p_times('Hello there', 5)
+
+def p_times_loop(str, n):
+    for i in range(n):
+        print(str)
+
+p_times('Goodbye', 5)
