a3c3ea0071bbd629468eedaa751af57268b67978
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-01-07 21:40:33 -0500

    [print_contacts] complete

--- exercises/03print_contacts.py
+++ exercises/03print_contacts.py
@@ -18,5 +18,18 @@
 contacts = {
   'Brian': '333-333-3333',
   'Lenny': '444-444-4444',
-  'Daniel': '777-777-7777'
+  'Daniel': '777-777-7777',
+  'Andrew': '999-999-9999',
+  'Amp': '888-888-8888',
+  'Bolt': '222-222-2222',
+  'Kristofer': '666-666-6666'
 }
+def print_contacts(dictionary):
+    for key in dictionary:
+        name = key
+        number = dictionary[key]
+        print(key + ' has a phone number of ' + number)
+
+print_contacts(contacts)
+
+# print(keylist)
